7f6e7083057baa80b4e6ac2f5291f09d9b421de0
davay <davay@defucc.me>
2024-11-28 15:36:30 +0700

    feat: lists of features

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -4,11 +4,7 @@
 
 <template lang="pug">
 .flex.flex-col.p-4.w-full.items-center.h-full.justify-center
-  .flex.flex-col.gap-2
-    .text-4xl Creative Multimedia Center
-    .text-2xl Place Coworking, Phuket
 
   content.prose
-    
     
 </template>
